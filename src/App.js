3a187f081a9e8d66f29a6084a3d412633e1da3fb
Kraae <117115729+Kraae@users.noreply.github.com>
2023-10-21 07:18:28 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DrawCardFromDeck from './DrawCardFromDeck';
+
+function App() {
+  return (
+    <div>
+      <DrawCardFromDeck />
+    </div>
+  );
+}
+
+export default App;
